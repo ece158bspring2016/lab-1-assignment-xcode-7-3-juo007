2b4e7e6c9afedcf48138294c316314a901536771
Hsu <jhsu@ussd-ofcmc5836.dynamic.ucsd.edu>
2016-04-19 20:46:13 -0700

    Lab 1

---
+++ Ratings/SampleData.swift
@@ -0,0 +1,12 @@
+//
+//  SampleData.swift
+//  Ratings
+//
+//  Created by Hsu, Jonathan on 4/18/16.
+//  Copyright © 2016 JamesOh. All rights reserved.
+//
+import Foundation
+let playersData = [
+    Player(name:"Bill Evans", game:"Tic-Tac-Toe", rating: 4),
+    Player(name: "Oscar Peterson", game: "Spin the Bottle", rating: 5),
+    Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2) ]
